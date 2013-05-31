ea3b721e2f2e6b6f0a42417e52add50d55498a28
rowend <rowend07@gmail.com>
2013-05-30 22:02:35 -0500

    basic functions

--- fixture.php
+++ fixture.php
@@ -1,3 +1,20 @@
 <?
+    if(!defined('BASEPATH'))
+        exit('No direct script access allowed');
 
+    class Fixture extends CI_Controller {
+
+        function index() {
+
+        }
+
+        function _user() {
+
+        }
+
+        function _books() {
+
+        }
+
+    }
 ?>
